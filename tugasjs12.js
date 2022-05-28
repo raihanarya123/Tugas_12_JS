6b939b53cee6083a37004899345c146e6ce8df9e
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:11:17 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs12.js
@@ -0,0 +1,9 @@
+function tugas12 (){
+    var buah = ["Apel", "Jeruk", "Mangga", "Pisang"];
+    console.log(buah);
+    buah.pop();
+    console.log(buah);
+    buah.shift();
+    console.log(buah);
+}
+tugas12();
